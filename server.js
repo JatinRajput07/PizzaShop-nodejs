8fbb31fe592e4fc7e72a03cbeb82008c2bde26b3
JatinRajput07 <kumarj597@gmail.com>
2020-11-18 15:05:18 +0530

    make home page

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
   res.render('home')
@@ -12,10 +17,11 @@ app.get('/',(req,res)=>{
 // set template engine
 app.use(expressLayout);
 app.set('views',path.join(__dirname, '/resources/views'))
-app.set('view engine','pug');
+// app.set('view engine','pug');
+app.set('view engine','ejs');
 
 
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT,'127.0.0.1',()=>{
   console.log(`hello from server port ${PORT}....`);
 })
