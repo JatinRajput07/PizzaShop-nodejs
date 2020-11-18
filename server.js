6c32b973e178c6dcd9dc87a2d4092db10987ca04
JatinRajput07 <kumarj597@gmail.com>
2020-11-19 01:23:49 +0530

    add cart page

--- server.js
+++ server.js
@@ -7,21 +7,35 @@ const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static('public'));
-
-app.get('/',(req,res)=>{
-  res.render('home')
-});
-
 
 // set template engine
 app.use(expressLayout);
-app.set('views',path.join(__dirname, '/resources/views'))
+app.set('views', path.join(__dirname, '/resources/views'))
 // app.set('view engine','pug');
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+});
+
+app.get('/cart', (req, res) => {
+  res.render('customers/cart')
+})
+app.get('/login', (req, res) => {
+  res.render('auth/login')
+})
+app.get('/register', (req, res) => {
+  res.render('auth/register')
+})
 
 
 
-app.listen(PORT,'127.0.0.1',()=>{
+
+
+
+app.listen(PORT, '127.0.0.1', () => {
   console.log(`hello from server port ${PORT}....`);
 })
